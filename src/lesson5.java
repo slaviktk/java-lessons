d3ecff0f476bb726cf056d889061a4df5c5fb6a5
slaviktk <slavikt9@gmail.com>
2018-05-19 12:37:21 +0300

    Выполнено успешно.

---
+++ src/lesson5.java
@@ -0,0 +1,36 @@
+/**
+ * Created by User on 19.05.2018.
+ */
+public class lesson5 {
+    public static void main (String [] args) {                 /* Primer 4*/
+    System.out.println(S(1,2));
+        System.out.println( S(7,8));
+        System.out.println(S(9,10));
+        System.out.println (stepen(5));
+        System.out.println (sin2(45));
+    }
+        public static int summa2 (int a, int b) {
+        int c = a + b;
+            return c;
+    }
+    public static double perimetr (double a, double b) {
+            return 2 * (a + b);
+    }
+    public static double f (double x) {
+            double y = x*x - 1;
+            return y;
+    }
+    public static double S (double a, double b){       /* Primer 1*/
+        double s = a*b;
+        return s;
+    }
+    public static double stepen (double x){            /* Primer 2*/
+        double y = Math.pow(x,3) + Math.pow(x,2) + 1;
+        return y;
+    }
+    public static double sin2 (double x){             /* Primer 3*/
+        double y = Math.sin(Math.toRadians(x*2));
+        return y;
+    }
+}
+
