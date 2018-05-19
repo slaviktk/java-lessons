b6acb8cac001d2c031b35d396dda90c43ffae78a
slaviktk <slavikt9@gmail.com>
2018-05-19 10:48:52 +0300

    1

---
+++ src/lesson4.java
@@ -0,0 +1,17 @@
+/**
+ * Created by User on 19.05.2018.
+ */
+public class lesson4 {
+    public static void main (String [] args){
+        int a=8;
+        long b=1024;
+        double c=8.0d;
+        int d=128;
+        boolean e=true;
+        System.out.println((b/c)/a);
+        System.out.println(e);
+
+
+
+    }
+}
