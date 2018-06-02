09b5dad42233cfac387108ab185db5539a0d7a9c
slaviktk <slavikt9@gmail.com>
2018-06-02 13:02:38 +0300

    123

--- Lesson5/src/Lesson7.java
+++ Lesson5/src/Lesson7.java
@@ -1,7 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created by User on 02.06.2018.
  */
 public class Lesson7 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+           //1.
+            System.out.println("Введите число А:");
+            int a = sc.nextInt();
+            System.out.println("Введите число B:");
+            int b = sc.nextInt();
+            System.out.println("A=" + a);
+            System.out.println("B=" + b);
+
+            if (a > b) {
+                System.out.println(a);
+            }
+                else  {
+                System.out.println(b);
+            }
+        }
+    }
+    public static int max (int a, int b, int c) {
+        if (a >= b) {
+            System.out.println(a);
+        } else  {
+            else return (b);
+        }
+
+    }
+
 }
--- Lesson5/src/lesson6.java
+++ Lesson5/src/lesson6.java
@@ -2,28 +2,32 @@
  * Created by User on 26.05.2018.
  */
 public class lesson6 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(usdTohrn(100));
-        System.out.println (S(5,8));
+        System.out.println(S(5, 8));
     }
-        public static double usdTohrn (double usd) {
+
+    public static double usdTohrn(double usd) {
         return usd * 26;
-        }
-        public static double hrnTousd (double hrn) {
-            return hrn / 26;
-        }
-        /*
-        1.Написать функцию, которая будет считать площадь прямоугольного треугольника или прямоугольника в зависимости
-        от стороны А (если четная, то треугольник, если нечетная то пряугольник). Функция принимает в себя два параметра А и Б (целочисленные).
-         */
-        public static double S (double a, double b) {
-            double s = a * b;
-            if (a % 2 == 0) {
-                return s / 2;
-            } else {
+    }
+
+    public static double hrnTousd(double hrn) {
+        return hrn / 26;
+    }
+
+    /*
+    1.Написать функцию, которая будет считать площадь прямоугольного треугольника или прямоугольника в зависимости
+    от стороны А (если четная, то треугольник, если нечетная то пряугольник). Функция принимает в себя два параметра А и Б (целочисленные).
+     */
+    public static double S(double a, double b) {
+        double s = a * b;
+        if (a % 2 == 0) {
             return s;
-            }
+        } else {
+            return s / 2;
         }
+    }
+}
 
         /*
         2.Написать функцию, которая выводит на экран в какой промежуток попадает число. Функция принимает один параметр
@@ -33,8 +37,3 @@ public class lesson6 {
              (10,100)
              (100, 1000)
         */
-            public static double promezutok {
-            double A;
-                if
-            }
-}
